ddb3939351a80afc7023fdcd496afae69fb70af5
BhaktiLead <bhaktileader@gmail.com>
2025-09-09 18:54:12 +0530

    Create model.py
    model.py

---
+++ model.py
@@ -0,0 +1,69 @@
+import torch
+self.pool1 = nn.MaxPool2d(2)
+self.encoder2 = ConvBlock(features[0], features[1])
+self.pool2 = nn.MaxPool2d(2)
+self.encoder3 = ConvBlock(features[1], features[2])
+self.pool3 = nn.MaxPool2d(2)
+self.encoder4 = ConvBlock(features[2], features[3])
+self.pool4 = nn.MaxPool2d(2)
+
+
+self.bottleneck = ConvBlock(features[3], features[3]*2)
+
+
+self.up4 = UpConv(features[3]*2, features[3])
+self.att4 = AttentionGate(F_g=features[3], F_l=features[3], F_int=features[3]//2)
+self.dec4 = ConvBlock(features[3]*2, features[3])
+
+
+self.up3 = UpConv(features[3], features[2])
+self.att3 = AttentionGate(F_g=features[2], F_l=features[2], F_int=features[2]//2)
+self.dec3 = ConvBlock(features[2]*2, features[2])
+
+
+self.up2 = UpConv(features[2], features[1])
+self.att2 = AttentionGate(F_g=features[1], F_l=features[1], F_int=features[1]//2)
+self.dec2 = ConvBlock(features[1]*2, features[1])
+
+
+self.up1 = UpConv(features[1], features[0])
+self.att1 = AttentionGate(F_g=features[0], F_l=features[0], F_int=features[0]//2)
+self.dec1 = ConvBlock(features[0]*2, features[0])
+
+
+self.final_conv = nn.Conv2d(features[0], out_channels, kernel_size=1)
+
+
+def forward(self, x):
+e1 = self.encoder1(x)
+e2 = self.encoder2(self.pool1(e1))
+e3 = self.encoder3(self.pool2(e2))
+e4 = self.encoder4(self.pool3(e3))
+
+
+b = self.bottleneck(self.pool4(e4))
+
+
+d4 = self.up4(b)
+e4_att = self.att4(g=d4, x=e4)
+d4 = self.dec4(torch.cat([d4, e4_att], dim=1))
+
+
+d3 = self.up3(d4)
+e3_att = self.att3(g=d3, x=e3)
+d3 = self.dec3(torch.cat([d3, e3_att], dim=1))
+
+
+d2 = self.up2(d3)
+e2_att = self.att2(g=d2, x=e2)
+d2 = self.dec2(torch.cat([d2, e2_att], dim=1))
+
+
+d1 = self.up1(d2)
+e1_att = self.att1(g=d1, x=e1)
+d1 = self.dec1(torch.cat([d1, e1_att], dim=1))
+
+
+out = self.final_conv(d1)
+out = torch.sigmoid(out)
+return out
